e1ada30bb1b174c16fa372a6eb8dff3f76b45fef
github username <github email address>
2020-03-16 15:40:49 -0400

    all methods functional

--- author.rb
+++ author.rb
@@ -1,1 +1,54 @@
 
+
+class Author
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        Author.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select do |book|
+            book.author == self.name
+        end
+    end
+
+    def write_book(title, word_count)
+        Book.new(self.name, title, word_count)
+    end
+
+    def total_words
+        total_words = 0
+
+        books.map do |book|
+            total_words += book.word_count
+        end
+
+        total_words
+
+    end
+
+    def self.most_words
+        most_words = 0
+        most_verbose = nil
+        
+        Author.all.each do |author|
+            if author.total_words > most_words
+                most_verbose = author
+                most_words = author.total_words
+            end
+        end
+        return most_verbose
+    end
+
+
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,23 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+tolkien = Author.new("J.R.R. Tolkien")
+josh = Author.new("Josh Allen")
+herbert = Author.new("Frank Herbert")
+
+tolkien.write_book("Lord of the Rings", 60000)
+tolkien.write_book("The Hobbit", 30000)
+
+josh.write_book("what", 200000)
+
+herbert.write_book("Dune", 100000)
+herbert.write_book("Chapterhouse: Dune", 20000)
+herbert.write_book("Dune Messiah", 30000)
+
+
+
 binding.pry
+
 
 
